b69b0049a544bc3ec90172b7a37cadb0d48499d2
alus <cunuukum@gmail.com>
2021-03-21 15:40:18 +0200

    Added versions

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-brew tap nlohmann/json
-brew install nlohmann_json
+#!/bin/sh
 
-brew install ncurses
+brew tap nlohmann/json
+
+brew install nlohmann-json@3.9
+
+brew install ncurses@6.2
+
 cmake .
 make all
